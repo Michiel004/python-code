75ec7e05ed598a82448e90ef17741de8cc9ff03c
Unknown <michiel.pieters44@telenet.be>
2018-05-28 10:47:58 -0700

    /

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import quandl
+import math
 
 df = quandl.get('WIKI/GOOGL')
 
 
 df = df[['Adj. Close']]
 print(df.head())
+
+forecast_col = 'Adj. close'
+df.fillna(-9999,inplace=True)
